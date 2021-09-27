90ac144c9489c97fc1544934cee86bd1bada82db
eballah92 <83521859+eballah92@users.noreply.github.com>
2021-09-27 11:40:12 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+
+ $title = 'index';
+ require_once 'includes/header.php';
+ 
+ ?>
+
+<h1 class="text-center">Registration for IT Conference</h1>
+
+   
+
+
+<?php  require_once 'includes/footer.php'; ?> 
