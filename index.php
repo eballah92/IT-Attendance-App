ae24cd7a27e6c5450f0c4adcab4f96ad9475bd59
eballah92 <83521859+eballah92@users.noreply.github.com>
2021-09-27 14:05:07 +0000

    added success page

--- index.php
+++ index.php
@@ -5,9 +5,62 @@
  
  ?>
 
+ <!-- 
+     -First Name
+     -Last Name
+     -Date of Birth (Use DatePicker)
+     -Specialty(Database Admin, Software Developer, Web Administrator)
+     -Email Address
+     -Contact Number
+ -->
+
 <h1 class="text-center">Registration for IT Conference</h1>
 
+<form method="get" action="success.php">
+    <div class="form-group">
+        <label for="firstname">First Name </label>
+        <input type="text" class="form-control" id="firstname" name="firstname">
+    </div>
+    <div class="form-group">
+        <label for="lastname">Last Name </label>
+        <input type="text" class="form-control" id="lastname"name="lastname">
+    </div>
+    <div class="form-group">
+        <label for="dob">Date of Birth </label>
+        <input type="text" class="form-control" id="dob" numfmt_parse_currency="dob">
+
+    <div class="form-group">
+    <label for="specialty">Area of Expertise</label>
+    <select class="form-control" id="specialty" name="specialty">
+      <option>Database Admin</option>
+      <option>Software Developer</option>
+      <option>Web Administrator</option>
+      <option>System Administrator</option>
+      <option>Other</option>
+    </select>
+    </div>
+    </div>
+    <div class="form-group">
+        <label for="Email">Email address</label>
+        <input type="email" class="form-control" id="Email" name="Email" aria-describedby="emailHelp">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    </div>
+    <div class="form-group">
+        <label for="Phone">Contact Number</label>
+        <input type="text" class="form-control" id="phone" aria-describedby="phoneHelp">
+        <small id="phoneHelp" name="phoneHelp" class="form-text text-muted">We'll never share your contact with anyone else.</small>
+    </div>
    
+     <button type="submit" name="submit" class="btn btn-primary btn-block">Submit</button>
+    </form>
+
+<br>
+
+<br>
+   
+<br>
+
+<br>
 
 
 <?php  require_once 'includes/footer.php'; ?> 
---
+++ success.php
@@ -0,0 +1,24 @@
+<?php 
+
+ $title = 'Success';
+ require_once 'includes/header.php';
+ 
+ ?>
+<h1 class="text-center text-success">You Have Been Registered!</h1>
+
+
+
+
+
+
+
+<br>
+
+<br>
+   
+<br>
+
+<br>
+
+
+<?php  require_once 'includes/footer.php'; ?> 
