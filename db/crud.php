54b229c7836608c5263f54c6d1b6a5c7ad299ea0
eballah92 <83521859+eballah92@users.noreply.github.com>
2021-10-02 10:01:41 +0000

    added delete page

--- db/crud.php
+++ db/crud.php
@@ -36,33 +36,28 @@
             }
         }
 
-        public function editAttendee($id,$fname, $lname, $dob, $email, $contact, $specialty){
-            try {
-                $sql = "UPDATE `attendee` SET `firstname`=:fname,`lastname`=:lname,
-                `dateofbirth`=:dod,`emailaddress`=:email,`contactnumber`=:contact,`specialty_id`=:specialty
-                WHERE attendee_id =:id ";
-    
-                $stmt = $this->db->prepare($sql);
-
-                //bind all paceholders to the actual values
-                $stmt->bindparam(':id',$id);
-                $stmt->bindparam(':fname',$fname);
-                $stmt->bindparam(':lname',$lname);
-                $stmt->bindparam(':dob',$dob);
-                $stmt->bindparam(':email',$email);
-                $stmt->bindparam(':contact',$contact);
-                $stmt->bindparam(':specialty',$specialty);
-    
-                // execute statement
+        public function editAttendee($id,$fname, $lname, $dob, $email,$contact,$specialty){
+            try{ 
+                 $sql = "UPDATE `attendee` SET `firstname`=:fname,`lastname`=:lname,`dateofbirth`=:dob,`emailaddress`=:email,`contactnumber`=:contact,`specialty_id`=:specialty WHERE attendee_id = :id ";
+                 $stmt = $this->db->prepare($sql);
+                 // bind all placeholders to the actual values
+                 $stmt->bindparam(':id',$id);
+                 $stmt->bindparam(':fname',$fname);
+                 $stmt->bindparam(':lname',$lname);
+                 $stmt->bindparam(':dob',$dob);
+                 $stmt->bindparam(':email',$email);
+                 $stmt->bindparam(':contact',$contact);
+                 $stmt->bindparam(':specialty',$specialty);
+ 
+                 // execute statement
                  $stmt->execute();
                  return true;
-    
-            } catch (PDOExeception $e) {
-                echo $e->getMessage();
-                return false;
+            }catch (PDOException $e) {
+             echo $e->getMessage();
+             return false;
             }
-           
-        }
+             
+         }
 
         public function getAttendees(){
             $sql = "SELECT * FROM `attendee` a inner join specialties s on a.specialty_id = s.specialty_id";
